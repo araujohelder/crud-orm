4b879f1ec42e65b4b4e9460c22c45642de05b06a
Helder Araujo <heldersilvaaraujo@gmail.com>
2018-01-25 06:40:14 -0200

    first commit

---
+++ teste.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: 'fernanda',
+    sobrenome: 'Almeida'
+}
+
+const nameWrite = ({nome, sobrenome}) => {
+    console.log(nome)
+    console.log(sobrenome)
+} 
+
+nameWrite(pessoa)
